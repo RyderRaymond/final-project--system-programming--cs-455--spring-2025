da3ae6103be605de6579e4f25fd7efbe40854b18
Ryder Raymond <117836603+RyderRaymond@users.noreply.github.com>
2025-05-14 18:47:54 -0400

    Clarify behavior of program 4

--- program-4.c
+++ program-4.c
@@ -11,6 +11,18 @@ extern char etext;
 extern char edata;
 extern char end;
 
+/** @brief Program 4: Prints end of segments in memory: text segment, uninitialized data segment, initialized data segment
+ *
+ *										The program also prints the locations of some static variables. Where they are stored is added in
+ *										comments after their declaration. When testing on Fedora, some of these matched with the real end
+ *										of segments, such as the memory after an uninitialized variable matching the end of the uninitialized data
+ *										segment, or &end. When testing on Ubuntu, some of these did not match up due to differing architecture.
+ *
+ *										Because of this discrepency, to note the real end of memory segments, use the addresses of the
+ *										extern variables etext, edata, and end, which are the final 3 lines printed.
+ *
+ * @return 0
+ */
 int main() {
 	char *allocated_string = malloc(1024);	//The pointer will be stored in the stack, the 1024 bytes in the heap
 	static int something_initialized = 7;				//This will be stored in the initialized data segment
