88cbf1740a274b026e45dbb3e255f3bb8e585352
Ryder Raymond <117836603+RyderRaymond@users.noreply.github.com>
2025-05-14 18:14:28 -0400

    Fix perror printing before printf

--- program-1.c
+++ program-1.c
@@ -46,7 +46,7 @@ int main(int argc, char *argv[]) {
   FILE *process_status_file = fopen(process_status_file_path, "r");
   if (process_status_file == NULL) {
     printf("Failed to open process status file: %s", process_status_file_path);
-    perror("");
+    printf("Likely that no process with this PID exists\n");
     exit(2);
   }
 
